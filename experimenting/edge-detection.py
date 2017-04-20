264d96bad4e17c69190247930b9b72ec8a93c767
ythuva <thuvany6@gmail.com>
2017-04-20 18:02:25 +0200

    pens
    First branch made by me
    yes

--- experimenting/edge-detection.py
+++ experimenting/edge-detection.py
@@ -8,16 +8,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Read image
-img = cv2.imread('ets4.jpg', 0)
+img = cv2.imread('ets.jpg', 0)
 
 # Run threshhold filter
 ret, thresh = cv2.threshold(img, 180, 255, cv2.THRESH_BINARY)
 
 # Add blur to fill "holes" in lines
 #blur = cv2.blur(img, (10,10))
-#kernel = np.ones((10,10),np.float32)/100
-#blur = cv2.filter2D(img,-1,kernel)
-blur = cv2.medianBlur(thresh, 11)
+kernel = np.ones((10,10),np.float32)/100
+blur = cv2.filter2D(thresh,-1,kernel)
+#blur = cv2.medianBlur(thresh, 11)
 
 # Detecte edges
 edges = cv2.Canny(blur, 150, 160)
