508470e97027f24e9e54423682122a92fe765158
Hans-Kristian <hkbruvold@gmail.com>
2017-04-21 21:12:29 +0200

    Added some more experimentation

--- experimenting/edge-detection4.py
+++ experimenting/edge-detection4.py
@@ -12,46 +12,49 @@ import math
 import matplotlib.pyplot as plt
 
 # Read image
-img = cv2.imread('ets.jpg')
+img = cv2.imread('ets3.jpg')
 
 # Open mask
-mask = cv2.imread('mask.png', 0)
+mask = cv2.imread('mask720.png', 0)
 
 # Convert to gray scale
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
+l = int(0.50*len(gray[0]))
+t = int(0.66*len(gray))
+m = cv2.mean(gray[t:t+5,l:l+5])[0]
+print(2*m)
 # Run threshhold filter
-ret, thresh = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY)
+ret, thresh = cv2.threshold(gray, 2*m, 255, cv2.THRESH_BINARY)
+#thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 7, 5)
+
+# Run another blur
+blur2 = cv2.GaussianBlur(thresh, (11, 11), 0)
+
+# Then a new threshold filter
+ret, thresh2 = cv2.threshold(blur2, 2, 255, cv2.THRESH_BINARY)
+
 
 # Add blur to fill "holes" in lines
 #blur = cv2.blur(img, (10,10))
 #kernel = np.ones((10,10),np.float32)/100
 #blur = cv2.filter2D(img,-1,kernel)
-blur = cv2.medianBlur(thresh, 11)
+blur = cv2.medianBlur(thresh2, 11)
 
-# Run another blur
-blur2 = cv2.blur(blur, (10,10))
-
-# Then a new threshold filter
-ret, thresh2 = cv2.threshold(blur2, 2, 255, cv2.THRESH_BINARY)
 
 # Apply mask to image
-masked = cv2.bitwise_and(thresh2, thresh2, mask=mask)
+masked = cv2.bitwise_and(blur, blur, mask=mask)
 
 # Detecte edges
 edges = cv2.Canny(masked, 150, 160, apertureSize=3)
 
 # Hough transform
-minLineLength = 100
+minLineLength = 70
 maxLineGap = 100
 lines = cv2.HoughLinesP(edges,1,np.pi/180,50,minLineLength=minLineLength,maxLineGap=maxLineGap)
 #lines = cv2.HoughLines(edges,1,np.pi/180,700)
 
-# left and right lines holder
-left = ()
-right = ()
 
-print(lines)
 # Draw lines from hough transform
 if lines != None:
     for line in lines:
@@ -59,18 +62,18 @@ if lines != None:
         a = math.atan2(y2-y1,x2-x1)
         if a<0:
             left = (x1,y1,x2,y2)
-            #cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+            cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
         else:
-            right = (x1,y1,x2,y2)
-            #cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+            right = (x1,y1,x2,y2)   
+            cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
         
         #print(x2-x1,y2-y1, math.atan2(y2-y1,x2-x1))
 else:
     print("NO LINES FOUND")
 
 
-cv2.line(img,(left[0],left[1]),(left[2],left[3]), (0,0,255),2)
-cv2.line(img,(right[0],right[1]),(right[2],right[3]), (0,255,0),2)
+#cv2.line(img,(left[0],left[1]),(left[2],left[3]), (0,0,255),2)
+#cv2.line(img,(right[0],right[1]),(right[2],right[3]), (0,255,0),2)
 
 
 #plt.subplot(121),plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
@@ -79,7 +82,7 @@ cv2.line(img,(right[0],right[1]),(right[2],right[3]), (0,255,0),2)
 #plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
 #plt.imshow(blur, cmap='gray')
-#plt.imshow(edges, cmap='gray')
+#plt.imshow(thresh2, cmap='gray')
 plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 #cv2.imshow('image',img)
 cv2.imwrite('houghlines5.jpg',img)
